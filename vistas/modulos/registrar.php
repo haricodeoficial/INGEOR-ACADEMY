f6e350fa20f9c25f3340a42b606f2c9c9a49de8b
haricodeoficial <34019072+JoseMan200124@users.noreply.github.com>
2022-12-21 00:56:58 -0600

    validacion formulario
    Adjunto de formulario de registro.

--- vistas/modulos/registrar.php
+++ vistas/modulos/registrar.php
@@ -7,7 +7,8 @@
                 <div class="col-lg-12 login-title">
                     REGISTRARSE
                 </div>
-
+                <form method="post"action="formulario.php" onsubmit="return registroUsuario()">
+          
                 <div class="col-lg-12 login-form">
                     <div class="col-lg-12 login-form">
                         <form>
@@ -15,28 +16,28 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label class="form-control-label">Nombre</label>
-                                    <input type="text" class="form-control">
+                                    <input type="text" class="form-control" name="regUsuario" id="regUsuario" require>
                                 </div>
                             </div>
                             <div class="col-md-6">
                             <div class="form-group">
                                     <label class="form-control-label">Apellidos</label>
-                                    <input type="text" class="form-control">
+                                    <input type="text" class="form-control" name="regUsuarioApellido" id="regUsuarioApellido" require>
                                 </div>
                             </div>
                             </div>
                            
                             <div class="form-group">
                                 <label class="form-control-label">Correo</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control"name="regEmail" id="regEmail" require>
                             </div>
                             <div class="form-group">
                                 <label class="form-control-label">Contraseña</label>
-                                <input type="password" class="form-control" i>
+                                <input type="password" class="form-control" name="regPassword" id="regPassword"require>
                             </div>
                             <div class="form-group">
                                 <label class="form-control-label">Confirmar contraseña</label>
-                                <input type="password" class="form-control" i>
+                                <input type="password" class="form-control" name="regConfirmacion" id="regConfirmacion" require>
                             </div>
                             <div class="col-lg-12 loginbttm">
                                 <div class="col-lg-12 login-btm login-text">
@@ -47,7 +48,7 @@
                                 <div class="col-md-12">
                                 <div class="checkBox">
                                         <label for="">
-                                            <input id="regTerminos" type="checkbox">
+                                            <input id="regPoliticas" type="checkbox">
                                             <small style="color: #fff;">
 
                                             Al registrarse, usted acepta nuestras condiciones de uso y políticas de privacidad. 
@@ -65,7 +66,7 @@
                         </form>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-2"></div>
+                </form>
             </div>
         </div>
 </div>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -221,6 +221,7 @@ var starRatingControl = new StarRating('.star-rating',{
 <script src="<?php echo $url;?>vistas/js/jBox.all.min.js" type="text/javascript"></script>
 <script src="<?php echo $url;?>vistas/js/demo.js" type="text/javascript"></script>
 <script src="<?php echo $url;?>vistas/js/check.js" type="text/javascript"></script>
+<script src="<?php echo $url;?>vistas/js/usuarios.js" type="text/javascript"></script>
 
 <script src="<?php echo $url;?>vistas/js/buscador.js" type="text/javascript"></script>
 
